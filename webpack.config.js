9f6d2db1149966d7ce2de944abcb2dbf43dcbb84
Todor Tsankov <tsankov.todya@gmail.com>
2017-02-14 16:20:20 +0100

    install & config stylesheets pipe loading

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
       }
     ]
   },
