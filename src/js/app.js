017629ceeddf2291fd6b0f2cacd7a3b8dcdd6f1a
Todor Tsankov <tsankov.todya@gmail.com>
2017-02-14 16:18:28 +0100

    add SCSS single point of entry

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '../scss/style.scss';
 const app = document.getElementById('app');
 
 ReactDOM.render(<h1>Book | Look</h1>, app);
