c2632cd8a71010e6ca0797779dd40a27cd45e3ae
Todor Tsankov <tsankov.todya@gmail.com>
2017-02-18 14:50:31 +0100

    add OneRoom atomic component

---
+++ src/components/Book/OneRoom.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react';
+
+const OneRoom = ({ name, title, callBookForm }) => (
+  <div class='col-md-3 col-sm-4 text-center'>
+    <div class='room'>
+      <div class='header'>
+        <span>{name}</span>
+      </div>
+      <div class='info'>
+        <p class='title'>{title}</p>
+        <button
+          class='btn btn-success btn-block'
+          onClick={callBookForm}>
+          BOOK
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+OneRoom.propTypes = {
+  name: PropTypes.string,
+  title: PropTypes.string,
+  callBookForm: PropTypes.func
+};
+
+export default OneRoom;
