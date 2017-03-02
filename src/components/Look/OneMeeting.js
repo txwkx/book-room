632a9d3ca752162fca6c81341f75d4014cb697f8
Todor Tsankov <tsankov.todya@gmail.com>
2017-03-02 23:17:06 +0100

    add OneMeeting atomic component

---
+++ src/components/Look/OneMeeting.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+
+const OneMeeting = ({roomName, title, host, timeStart, timeEnd, status, toggleMeetingStatus}) => (
+
+  <div class={`meeting ${status ? 'occupied' : 'available'}`}>
+    <p>{roomName}
+      <span class='time'>{timeStart} - {timeEnd}</span>
+    </p>
+    <h4 class='title'>{title}</h4>
+    <p class='author'>
+      <span>Host: </span>{host}
+      </p>
+    { status ?
+      <a class='attend' onClick={toggleMeetingStatus}>Cancel</a> :
+      <a class='attend free' onClick={toggleMeetingStatus}>Attend</a>
+    }
+  </div>
+  
+);
+
+OneMeeting.propTypes = {
+  roomName: PropTypes.string,
+  title: PropTypes.string,
+  host: PropTypes.string,
+  time: PropTypes.string,
+  status: PropTypes.bool,
+  toggleMeetingStatus: PropTypes.func
+};
+
+export default OneMeeting;
