5c1eff44e5b536bc5e35ab4820a1d69d4f0f5f09
Todor Tsankov <tsankov.todya@gmail.com>
2017-03-02 23:18:12 +0100

    add MeetingsList component

---
+++ src/components/Look/MeetingsList.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+
+import OneMeeting from './OneMeeting';
+
+export default class MeetingsList extends Component {
+
+  render(){
+
+    return(
+      <div class='timeline-body'>
+        <div class='container'>
+
+          <OneMeeting
+              roomName={'Meeting Hall #1'}
+              title={'Small Talk'}
+              host={'Major'}
+              timeStart={'9:00'}
+              timeEnd={'10:00'}
+              status={true}
+              toggleMeetingStatus={this.props.toggleMeetingStatus}
+              />
+
+          <div class='date text-center'>
+            <p>31 February</p>
+          </div>
+
+          <OneMeeting
+              roomName={'Room #5'}
+              title={'True meeting'}
+              host={'Name Surname'}
+              timeStart={'19:00'}
+              timeEnd={'23:00'}
+              status={false}
+              toggleMeetingStatus={this.props.toggleMeetingStatus}
+              />
+
+        </div>
+      </div>
+    );
+  }
+}
+
+MeetingsList.propTypes = {
+  toggleMeetingStatus: PropTypes.func
+};
