04ce2654ed6f06c8d2e13e3068453f9b21d14940
Todor Tsankov <tsankov.todya@gmail.com>
2017-04-10 10:33:42 +0200

    add single room route handler + update syntax

--- server/roomRouter.js
+++ server/roomRouter.js
@@ -7,14 +7,20 @@ const Room = mongoose.model('Room', roomSchema);
 
 const roomRouter = express.Router();
 
-roomRouter.route('/')
-  .get((req, res) => {
-
+roomRouter.get('/', (req, res) => {
     let query = {};
 
     Room.find(query, (err, rooms) => {
       if(err) res.status(500).send(err);
       else res.json(rooms);
+    });
+  });
+
+roomRouter.get('/:id', (req, res) => {
+    Room.findById(req.params.id, (err, room) => {
+      if(err) res.status(500).send(err);
+      if(!room) res.status(404).send('Room not found.');
+      res.status(200).json(room);
     });
   });
 
