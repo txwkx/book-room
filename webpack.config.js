e87c3af3bb1bd351bd2e3d5ff888fa6398a3a06e
Todor Tsankov <tsankov.todya@gmail.com>
2017-04-19 14:38:49 +0200

    separate babel config

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react', 'stage-0'],
-          plugins: ['react-html-attrs']
-        }
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
