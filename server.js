6fca081a39efd623ee5d87fc6f0898d208306b6a
Todor Tsankov <tsankov.todya@gmail.com>
2017-04-29 18:24:47 +0200

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8008;
 const app = express();
+
+mongoose.connect('mongodb://localhost/bookroomAPI', (err) => {
+  if(err) throw err;
+});
 
 app.get('/', (req, res) => res.send('Welcome to the "BOOK ROOM" API'));
 
