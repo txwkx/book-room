5611a026a6cf7ffde9fcc8c5134d81b32734b042
Todor Tsankov <tsankov.todya@gmail.com>
2017-04-29 18:25:22 +0200

    add middleware

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ mongoose.connect('mongodb://localhost/bookroomAPI', (err) => {
   if(err) throw err;
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => res.send('Welcome to the "BOOK ROOM" API'));
 
 const roomRouter = require('./server/roomRouter');
