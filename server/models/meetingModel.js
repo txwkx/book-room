a25c239e0cb76367856b2caa922be353f9093c2e
Todor Tsankov <tsankov.todya@gmail.com>
2017-05-02 12:29:46 +0200

    add Meeting model

---
+++ server/models/meetingModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
+
+const meetingSchema = new Schema({
+  roomId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Room'
+  },
+  title: String,
+  hostId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  startTime: { type: Date },
+  endTime: { type: Date },
+  attendees: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+module.exports = mongoose.model('Meeting', meetingSchema);
