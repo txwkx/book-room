47389d12445f19b754bf8dabcacebed7a3759770
Todor Tsankov <tsankov.todya@gmail.com>
2017-05-13 22:18:52 +0200

    add Booking router

---
+++ server/bookingRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const moment = require('moment');
+const mongoose = require('mongoose'),
+      ObjectId = mongoose.Types.ObjectId;
+const booking = require('./models/bookingModel');
+
+const bookingRouter = express.Router();
+
+bookingRouter.get('/:id/:date', (req, res) => {
+    let query = {
+      roomId: req.params.id,
+      date: {$gte: req.params.date, $lt: moment(req.params.date).add(1, 'day').toISOString() }
+    };
+
+    booking.find(query, (err, bookings) => {
+      if(err) res.status(500).send(err);
+      if(!bookings) res.status(404).send('Booking not found.');
+      res.status(200).json(bookings);
+    });
+
+});
+
+module.exports = bookingRouter;
