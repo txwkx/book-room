234f59ce62c6948b4faeb4902cc77b3237f722e5
Todor Tsankov <tsankov.todya@gmail.com>
2017-06-23 19:02:17 +0200

    include libs and strategies into the server config

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express'),
-      mongoose = require('mongoose'),
-      bodyParser = require('body-parser'),
-      expressValidator = require('express-validator');
+const express          = require('express'),
+      mongoose         = require('mongoose'),
+      bodyParser       = require('body-parser'),
+      morgan           = require('morgan'),
+      expressValidator = require('express-validator'),
+      passport         = require('passport'),
+      session          = require('express-session');
 
 const port = process.env.PORT || 8008;
 const app = express();
@@ -11,9 +14,14 @@ mongoose.connect('mongodb://localhost/bookroomAPI', (err) => {
   if(err) throw err;
 });
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
+
+app.use(session({ secret: 'wannalookorbook', resave: true, saveUninitialized: true }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -23,7 +31,11 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => res.send('Welcome to the "BOOK ROOM" API'));
 
+require('./server/passport/local-signup')(passport);
+require('./server/passport/local-login')(passport);
+
 require('./server/authRouter')(app, passport);
+
 const roomRouter = require('./server/roomRouter');
 app.use('/api/rooms', roomRouter);
 
