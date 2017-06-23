b87b3a833cbf2cf91896a6477f2f48726ad111e5
Todor Tsankov <tsankov.todya@gmail.com>
2017-06-23 18:52:51 +0200

    improve User model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose'),
-      Schema = mongoose.Schema;
+      Schema   = mongoose.Schema,
+      bcrypt   = require('bcrypt');
 
 const userSchema = new Schema({
-  name: String
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+});
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt((saltError, salt) => {
+    if (saltError) return next(saltError);
+
+    bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) return next(hashError);
+
+      user.password = hash;
+      next();
+
+    });
+  });
+
 });
 
 module.exports = mongoose.model('User', userSchema);
