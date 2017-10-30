57cdc39d6aec98e4fa750a845e06cb1433f311a8
Todor Tsankov <tsankov.todya@gmail.com>
2017-10-30 10:40:15 +0100

    update 50/50 page text

--- src/components/Pick/Pick.js
+++ src/components/Pick/Pick.js
@@ -6,14 +6,14 @@ export default () => (
     <Link id='look' to="/look">
       <span class='text'>
         <span class='heading'>LOOK</span>
-        <span>Scroll through the upcoming events</span>
+        <span>Upcoming Events</span>
         <i class='icon'></i>
       </span>
     </Link>
     <Link id='book' to="/book">
       <span class='text'>
         <span class='heading'>BOOK</span>
-        <span>Browse through a list of rooms</span>
+        <span>List of Rooms</span>
         <i class='icon'></i>
       </span>
     </Link>
